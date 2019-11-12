c92bc7f6bdecbf3b99dffd66fc3ba63460481d9e
legiera <legierska.ana@gmail.com>
2019-11-13 00:39:17 +0100

    ok & cancel button working

--- src/main/java/pl/edu/agh/iisg/to/javafx/cw2/presenter/TransactionEditDialogPresenter.java
+++ src/main/java/pl/edu/agh/iisg/to/javafx/cw2/presenter/TransactionEditDialogPresenter.java
@@ -5,7 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.converter.LocalDateStringConverter;
+import pl.edu.agh.iisg.to.javafx.cw2.model.Category;
 import pl.edu.agh.iisg.to.javafx.cw2.model.Transaction;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TransactionEditDialogPresenter {
 
@@ -39,10 +47,16 @@ public class TransactionEditDialogPresenter {
 	public boolean isApproved() {
 		return approved;
 	}
-	
+
 	@FXML
 	private void handleOkAction(ActionEvent event) {
-		// TODO: implement
+		if(isInputValid()){
+			approved = true;
+			updateModel();
+			dialogStage.close();
+		} else {
+
+		}
 	}
 	
 	@FXML
@@ -54,12 +68,43 @@ public class TransactionEditDialogPresenter {
 		// TODO: implement
 		return true;
 	}
-	
+
 	private void updateModel() {
-		// TODO: implement
+		transaction.setDate(stringToDate(dateTextField.getText()));
+		transaction.setPayee(payeeTextField.getText());
+		transaction.setCategory(new Category(categoryTextField.getText()));
+		BigDecimal inflow = new BigDecimal(0);
+		try {
+			inflow = stringToBigDecimal(inflowTextField.getText());
+		} catch (ParseException e){
+			e.printStackTrace();
+		}
+		transaction.setInflow(inflow);
 	}
-	
+
 	private void updateControls() {
-		// TODO: implement
+		dateTextField.setText(dateToString(transaction.getDate()));
+		payeeTextField.setText(transaction.getPayee());
+		categoryTextField.setText(transaction.getCategory().getName());
+		inflowTextField.setText(transaction.getInflow().toString());
+	}
+
+	private String dateToString(LocalDate date){
+		String pattern = "yyyy-MM-dd";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+		LocalDateStringConverter converter = new LocalDateStringConverter(formatter, formatter);
+		return converter.toString(date);
+	}
+
+	private LocalDate stringToDate(String date){
+		String pattern = "yyyy-MM-dd";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+		LocalDateStringConverter converter = new LocalDateStringConverter(formatter, formatter);
+		return converter.fromString(date);
+	}
+	private BigDecimal stringToBigDecimal(String number) throws ParseException {
+		DecimalFormat decimalFormatter = new DecimalFormat();
+		decimalFormatter.setParseBigDecimal(true);
+		return (BigDecimal) decimalFormatter.parse(number);
 	}
 }
